7757e9566c89807b212171bcc01ef581a4dbf13c
XanderXueMingJi <xueming.ji@qq.com>
2018-02-04 11:28:48 +0800

    02
    02

--- CSDN/csdn.js
+++ CSDN/csdn.js
@@ -1,5 +1,10 @@
-var ad = document.querySelectorAll('.pull-left');
-ad = Array.from(ad);
-ad.filter(function (value){
+var arr = [];
+
+//csdn  前端简单说
+arr.push.apply(arr, Array.prototype.slice.call(document.getElementsByClassName('csdn-toolbar-skin-black')));
+arr.push.apply(arr, Array.prototype.slice.call(document.getElementsByClassName('pulllog-box')));
+arr.push.apply(arr, Array.prototype.slice.call(document.getElementsByTagName("aside")));
+arr.filter(function (value){
    value.style.display = 'none';
 });
+
